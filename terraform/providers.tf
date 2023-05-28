78ebd449a0bacbb2cae732080214bcaa3d78aeee
Yura-S <36129839+Yura-S@users.noreply.github.com>
2023-05-28 18:54:09 +0400

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -5,6 +5,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket         = "bucketforbackend280523"
+    key            = "my-terraform-state/terraform.tfstate"
     region         = "us-east-1"
   }
 }
